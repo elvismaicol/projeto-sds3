1e5a827aae3159fd951953e376e91a4ca333cae7
Elvis Maicol <elvismaicol@gmail.com>
2021-05-08 23:57:41 -0300

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -12,8 +12,8 @@ export type Sale = {
 export type SalePage = {
     content?: Sale[];
     last: boolean,
-    totalPages: number;
     totalElements: number;
+    totalPages: number;
     size?: number;
     number: number;
     first: boolean;
